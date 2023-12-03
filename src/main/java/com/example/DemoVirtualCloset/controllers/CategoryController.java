6afa7ae3397e5833c84a004e717413132751b5c1
VHriho <v12grig12@gmail.com>
2023-12-03 17:53:30 +0200

    added page for additing category

--- src/main/java/com/example/DemoVirtualCloset/controllers/CategoryController.java
+++ src/main/java/com/example/DemoVirtualCloset/controllers/CategoryController.java
@@ -1,25 +1,32 @@
-//package com.example.DemoVirtualCloset.controllers;
-//
-//import com.example.DemoVirtualCloset.services.CategoryService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@Controller
-//public class CategoryController {
-//
-//    private CategoryService categoryService;
-//
-//    @Autowired
-//    public CategoryController(CategoryService categoryService) {
-//        this.categoryService = categoryService;
-//    }
-//
-//    @GetMapping("/category-view")
-//    public ModelAndView index() {
-//        ModelAndView modelAndView = new ModelAndView("index");
-//        modelAndView.addObject("categories", categoryService.findAll());
-//        return modelAndView;
-//    }
-//}
+package com.example.DemoVirtualCloset.controllers;
+
+import com.example.DemoVirtualCloset.dto.CategoryDto;
+import com.example.DemoVirtualCloset.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/create-category")
+    public String showCreateCategoryForm(CategoryDto categoryDto) {
+        return "newCategoryForm";
+    }
+
+    @PostMapping("/create")
+    public String createCategory(@Valid CategoryDto categoryDto) {
+        categoryService.save(categoryDto.getName());
+        return "redirect:/category-view";
+    }
+}
